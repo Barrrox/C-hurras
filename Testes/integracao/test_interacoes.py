b3b92f5f19de77f6cbfbbb022dce6184868708c0
Barrrox <matheusbarros173@gmail.com>
2026-04-24 16:54:12 -0300

    ADD: Casos de teste com mais tokens

--- Testes/integracao/test_interacoes.py
+++ Testes/integracao/test_interacoes.py
@@ -1,6 +1,5 @@
 import pytest
 import itertools
-import analisador_lexico
 from testes.utils import executar_lexico, validar_token
 
 # Amostras de tokens para as combinações
@@ -26,10 +25,18 @@ def test_interacao_com_espaco(tk1, tk2):
     validar_token(tokens[1], txt2, tipo2)
 
 @pytest.mark.parametrize("tk1, tk2", [
-    (("+", "op"), ("123", "int")),     # +123
-    ((";", ";"), ("vaca", "vaca")),    # ;vaca
-    (("{", "{"), ("id", "id")),        # {id
-    (("123", "int"), (";", ";")),      # 123;
+    (("+", "op"), ("123", "int")),      # Operator + Number (+123)
+    (("123", "int"), ("+", "op")),      # Number + Operator (123+)
+    (("vaca", "vaca"), (";", ";")),     # ID + Semicolon (vaca;)
+    ((";", ";"), ("vaca", "vaca")),     # Semicolon + ID (;vaca)
+    (("+", "op"), ("vaca", "vaca")),    # Operator + ID (+vaca)
+    (("vaca", "vaca"), ("+", "op")),    # ID + Operator (vaca+)
+    (("{", "{"), ("123", "int")),       # Delimiter + Number ({123)
+    (("123", "int"), ("}", "}")),       # Number + Delimiter (123})
+    (("id_teste", "id"), (";", ";")),   # ID + Semicolon (id_teste;)
+    ((";", ";"), ("id_teste", "id")),   # Semicolon + ID (;id_teste)
+    (("(", "("), ("123", "int")),       # Delimiter + Number ((123)
+    (("123", "int"), (")", ")")),       # Number + Delimiter (123))
 ])
 def test_interacao_sem_espaco(tk1, tk2):
     """Testa tokens colados que devem ser distinguidos pelo lexer."""
@@ -42,6 +49,37 @@ def test_interacao_sem_espaco(tk1, tk2):
     assert len(tokens) == 2
     validar_token(tokens[0], txt1, tipo1)
     validar_token(tokens[1], txt2, tipo2)
+
+def test_tres_tokens_colados():
+    """Garante que 123+vaca gera 3 tokens."""
+    codigo = "123+vaca"
+    tokens = executar_lexico(codigo)
+    
+    assert len(tokens) == 3
+    validar_token(tokens[0], "123", "int")
+    validar_token(tokens[1], "+", "op")
+    validar_token(tokens[2], "vaca", "vaca")
+
+def test_id_ponto_virgula_numero():
+    """vaca;123 gera 3 tokens."""
+    codigo = "vaca;123"
+    tokens = executar_lexico(codigo)
+    
+    assert len(tokens) == 3
+    validar_token(tokens[0], "vaca", "vaca")
+    validar_token(tokens[1], ";", ";")
+    validar_token(tokens[2], "123", "int")
+
+def test_delimitadores_aninhados():
+    """Garante que (()) gera 4 tokens de parênteses."""
+    codigo = "(())"
+    tokens = executar_lexico(codigo)
+    
+    assert len(tokens) == 4
+    validar_token(tokens[0], "(", "(")
+    validar_token(tokens[1], "(", "(")
+    validar_token(tokens[2], ")", ")")
+    validar_token(tokens[3], ")", ")")
 
 def test_id_colado_com_numero():
     """vaca123 deve ser um único ID, não 'vaca' + '123'"""
