38c5b0ba660c110394b967d4cd8864baa03d643b
LoraLeonarda <lora.leonarda77@gmail.com>
2026-08-11 20:28:47 -0300

    UPDATE: implementado o método construir_tabelaSLR() da classe automato

--- src/construtor_tabelaSLR.py
+++ src/construtor_tabelaSLR.py
@@ -1,6 +1,6 @@
 class SLRcell:
     def __init__(self, tipo, valor):
-        self.tipo = tipo   # empilha, reduz, aceita, errovazio, erroreduz -> 0, 1, 2, 3, 4
+        self.tipo = tipo   # empilha, reduz, aceita, errovazio, erroreduz, goto -> 0, 1, 2, 3, 4, 5
         self.valor = valor # valor numerico referente a acao
 
 class ConstrutorTabelaSLR:
@@ -33,30 +33,66 @@ class ConstrutorTabelaSLR:
 
         O que retorna: Nada. Altera atributos de instância.
         """
-
+        
         """
-Entrada: Gramática aumentada G’
-Saída: Funções sintáticas SLR ação e desvio para G’
-Proc tabela(G’) {
-
-1. Construir C={I0..In} //coleção de itens para G’
-
-2. O estado i é construído a partir de Ii. As ações sintáticas para o estado i são determinadas como segue:
-    a) Se [A → α•aβ] estiver em Ii e desvio(Ii, a)=Ij, então estabelecer ação[i,a] em “empilha j”. Aqui a é terminal. //R1
-    b)Se [A → α•] estiver em Ii, então estabelecer ação[i,a] em “reduzir através de A→ α” para todo a em Follow(A). Aqui A é diferente de S’. //R2
-    c)Se[S’ → S•] estiver em Ii, então estabelecer ação[i,$] igual a “aceitar”. //R3
-
-3. As transições de desvio para o estado i são construídos pra todos os não-terminais A usando a regra: se desvio(Ii, A) = Ij, então desvio[i,A] = j //R4
-
-4. Entradas não definidas: erro
-
-}
+        Entrada: Gramática aumentada G’
+        Saída: Funções sintáticas SLR ação e desvio para G’
+        Proc tabela(G’)
+        {
+            1. Construir C = {I0, I1, ..., In}   // coleção de itens LR(0) para G’
+            2. O estado i é construído a partir de Ii. As ações sintáticas para o estado i são determinadas como segue:
+                a) Se [A → α•aβ] está em Ii e desvio(Ii, a) = Ij, então fazer ação[i, a] = “empilha j”. Aqui a é um terminal.   // R1
+                b) Se [A → α•] está em Ii, então fazer ação[i, a] = “reduzir A → α” para todo a ∈ Follow(A). Aqui A ≠ S’.   // R2
+                c) Se [S’ → S•] está em Ii, então fazer ação[i, $] = “aceitar”.   // R3
+            3. As transições de desvio para o estado i são construídas para todos os não-terminais A usando a regra: se desvio(Ii, A) = Ij, então desvio[i, A] = j.   // R4
+            4. Entradas não definidas nas tabelas correspondem a erro.
+        }
         """
 
         # Inicializar tabela SLR com N linhas (N estados do automato) e com T + A colunas (T terminais + A não terminais -> Seguindo regra 4 dos slides na construção da tabela)
         tabelaSLR = {}
         
-        pass
+        
+        # Para cada estado
+        for estado in automato.estados:
+            # Para cada produção
+            for item in estado.fechamento:
+                # Obtem simbolo lido
+                simbolo_lido = item.ponto_dir
+                producao = (item.esq, item.dir)
+                
+                # Se lê um terminal
+                if simbolo_lido in gramatica.terminais:
+                    # Coloca em tabela[estado, terminal] um empilhar estado_alvo
+                    tabelaSLR[estado.id, gramatica.simbolos(simbolo_lido)] = SLRcell(0, automato.transicoes((estado.id, simbolo_lido)))
+                    
+                # Se o ponto_dir == None e também é a produção 0 da gramatica
+                elif (simbolo_lido == None) and (producao == gramatica.producoes[0]):
+                    # Coloca em tabela[estado, $] o aceita
+                    tabelaSLR[estado.id, gramatica.simbolos['$']] = SLRcell(2, -1)
+                    
+                # Se o ponto_dir == None
+                elif (simbolo_lido == None):
+                    # Coloca no follow de gramatica.esq da produção a redução (própria produção)
+                    producao_index = -1
+                    for i in range(len(gramatica.producoes)):
+                        prod = gramatica.producoes[i]
+                        if prod == producao:
+                            producao_index = i
+                            break
+                    for flw in gramatica.follow[simbolo_lido]:
+                        tabelaSLR[estado.id, gramatica.simbolos(flw)] = SLRcell(1, producao_index)
+                    
+                # Se lê um não-terminal
+                elif simbolo_lido in gramatica.nao_terminais:
+                    # Coloca em tabela[estado, não-terminal] o goto estado_alvo
+                    tabelaSLR[estado.id, gramatica.simbolos(simbolo_lido)] = SLRcell(5, automato.transicoes((estado.id, simbolo_lido)))
+                
+                # Se isso rodar, algo deu errado
+                else:
+                    print("a tabela não ta indo, ta lendo coisa que não devia, suspeito...")
+                    return
+        
 
     def exportar_json(self, caminho="tabela_slr.json") -> None:
         """
